5a8fbae3b2542456a5e566c288a3f76f78ac885b
Iohan <iohangp@gmail.com>
2018-11-07 14:50:59 -0200

    Alterações MunicipioLocalidade

--- Avalia_Pesquisa/Services/CloudDataStore.cs
+++ Avalia_Pesquisa/Services/CloudDataStore.cs
@@ -28,53 +28,53 @@ namespace Avalia_Pesquisa
         public bool MunicipiosSync(string chave)
         {
             
-        //    if (!CrossConnectivity.Current.IsConnected)
-        //        return false;
+            if (!CrossConnectivity.Current.IsConnected)
+                return false;
 
-        //    List<Municipio> municipio = new List<Municipio>
-        //    {
-        //        new Municipio(){ IdMunicipio =1,Descricao="Ponta Grossa" },
-        //        new Municipio(){ IdMunicipio =2,Descricao="Curitiba" },
-        //    };
-        //    var serializedMunicipio = JsonConvert.SerializeObject(municipio);
+            List<Municipio> municipio = new List<Municipio>
+            {
+                new Municipio(){ IdMunicipio =1,Descricao="Ponta Grossa" },
+                new Municipio(){ IdMunicipio =2,Descricao="Curitiba" },
+            };
+            var serializedMunicipio = JsonConvert.SerializeObject(municipio);
 
-        //    List<Municipio_Localidade> municipio_localidade = new List<Municipio_Localidade>
-        //    {
-        //        new Municipio_Localidade(){ IdLocalidade =1,Descricao="Cará-Cará",IdMunicipio=1},
-        //        new Municipio_Localidade(){ IdLocalidade =2,Descricao="Água Verde",IdMunicipio=2 },
-        //    };
-        //    var serializedLocal = JsonConvert.SerializeObject(municipio_localidade);
+            List<Municipio_Localidade> municipio_localidade = new List<Municipio_Localidade>
+            {
+                new Municipio_Localidade(){ IdLocalidade =1,Descricao="Cará-Cará",IdMunicipio=1},
+                new Municipio_Localidade(){ IdLocalidade =2,Descricao="Água Verde",IdMunicipio=2 },
+            };
+            var serializedLocal = JsonConvert.SerializeObject(municipio_localidade);
 
-        //    try
-        //    {
-        //        using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "AvaliaPesquisa.db")))
-        //        {
-        //            foreach (var elemento in municipio)
-        //            {
-        //                var dados = conexao.Query<Config>("SELECT * FROM Municipio Where IdMunicipio=?", elemento.IdMunicipio);
+            try
+            {
+                using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "AvaliaPesquisa.db")))
+                {
+                    foreach (var elemento in municipio)
+                    {
+                        var dados = conexao.Query<Municipio>("SELECT * FROM Municipio Where IdMunicipio=?", elemento.IdMunicipio);
 
-        //                if(dados.Count == 0)
-        //                {
-        //                    conexao.Query<Config>("INSERT INTO Municipio (IdMunicipio,Descricao) Values(?,?)", elemento.IdMunicipio, elemento.Descricao);
-        //                }
-        //            }
+                        if(dados.Count == 0)
+                        {
+                            conexao.Query<Municipio>("INSERT INTO Municipio (IdMunicipio,Descricao) Values(?,?)", elemento.IdMunicipio, elemento.Descricao);
+                        }
+                    }
 
-        //            foreach (var local in municipio_localidade)
-        //            {
-        //                var dadosLocal = conexao.Query<Config>("SELECT * FROM Municipio_Localidade Where idLocalidade=?", local.IdLocalidade);
+                    foreach (var local in municipio_localidade)
+                    {
+                        var dadosLocal = conexao.Query<Municipio_Localidade>("SELECT * FROM Municipio_Localidade Where idLocalidade=?", local.IdLocalidade);
 
-        //                if (dadosLocal.Count == 0)
-        //                {
-        //                    conexao.Query<Config>("INSERT INTO Municipio_Localidade (IdLocalidade,IdMunicipio,Descricao) Values(?,?,?)",
-        //                                          local.IdLocalidade, local.IdMunicipio, local.Descricao);
-        //                }
-        //            }
-        //        }
-        //    }
-        //    catch (SQLiteException ex)
-        //    {
-        //        return false;
-        //    }
+                        if (dadosLocal.Count == 0)
+                        {
+                            conexao.Query<Municipio_Localidade>("INSERT INTO Municipio_Localidade (IdLocalidade,IdMunicipio,Descricao) Values(?,?,?)",
+                                                  local.IdLocalidade, local.IdMunicipio, local.Descricao);
+                        }
+                    }
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                return false;
+            }
 
             return true;
  
@@ -82,36 +82,36 @@ namespace Avalia_Pesquisa
 
         public bool UsuarioSync(string chave)
         {
-        //    if (!CrossConnectivity.Current.IsConnected)
-        //        return false;
+            if (!CrossConnectivity.Current.IsConnected)
+                return false;
 
-        //    List<Usuario> usuario = new List<Usuario>
-        //    {
-        //        new Usuario(){ IdUsuario =1,Nome="Iohan Pierdoná",Cpf="07547555926",Senha="123456" },
-        //        new Usuario(){ IdUsuario =2,Nome="Pedro Barros",Cpf="05404781998",Senha="123456" },
-        //        new Usuario(){ IdUsuario =2,Nome="Michael bereza",Cpf="05892131998",Senha="1" },
-        //    };
+            List<Usuario> usuario = new List<Usuario>
+            {
+                new Usuario(){ IdUsuario =1,Nome="Iohan Pierdoná",Cpf="07547555926",Senha="123456" },
+                new Usuario(){ IdUsuario =2,Nome="Pedro Barros",Cpf="05404781998",Senha="123456" },
+                new Usuario(){ IdUsuario =2,Nome="Michael bereza",Cpf="05892131998",Senha="1" },
+            };
 
-        //    try
-        //    {
-        //        using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "AvaliaPesquisa.db")))
-        //        {
-        //            foreach (var elemento in usuario)
-        //            {
-        //                var dados = conexao.Query<Config>("SELECT * FROM Usuario Where IdUsuario=?", elemento.IdUsuario);
+            try
+            {
+                using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "AvaliaPesquisa.db")))
+                {
+                    foreach (var elemento in usuario)
+                    {
+                        var dados = conexao.Query<Usuario>("SELECT * FROM Usuario Where IdUsuario=?", elemento.IdUsuario);
 
-        //                if (dados.Count == 0)
-        //                {
-        //                    conexao.Query<Config>("INSERT INTO Usuario (IdUsuario,Nome,Cpf,Senha) Values(?,?,?,?)", 
-        //                                            elemento.IdUsuario, elemento.Nome, elemento.Cpf, elemento.Senha);
-        //                }
-        //            }
-        //        }
-        //    }
-        //    catch (SQLiteException ex)
-        //    {
-        //        return false;
-        //    }
+                        if (dados.Count == 0)
+                        {
+                            conexao.Query<Usuario>("INSERT INTO Usuario (IdUsuario,Nome,Cpf,Senha) Values(?,?,?,?)", 
+                                                    elemento.IdUsuario, elemento.Nome, elemento.Cpf, elemento.Senha);
+                        }
+                    }
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                return false;
+            }
 
 
             return true;
