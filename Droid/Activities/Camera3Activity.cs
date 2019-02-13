3238a5b9b1f2f5ff4d884c7a51e5c9431232028e
Michael <m1xaeu@hotmail.com>
2019-02-13 13:28:51 -0200

    ajustes camera

---
+++ Droid/Activities/Camera3Activity.cs
@@ -0,0 +1,58 @@
+﻿
+using Plugin.Media;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using System;
+using Plugin.Media.Abstractions;
+using Android.Widget;
+
+namespace Avalia_Pesquisa.Droid.Activities
+{
+    [Activity(Label = "Camera3Activity")]
+    public class Camera3Activity : BaseActivity
+    {
+
+        ImageView imageView;
+        protected override int LayoutResource => Resource.Layout.Camera;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //SetContentView(Resource.Layout.Camera);
+            var btnCamera = FindViewById<Button>(Resource.Id.btnCamera);
+            imageView = FindViewById<ImageView>(Resource.Id.imageView);
+            btnCamera.Click += TirarFoto;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        private async void TirarFoto(object sender, EventArgs e)
+        {
+
+            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
+            {
+                var mediaOptions = new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = $"{DateTime.UtcNow}.jpg"
+                };
+
+
+                // Take a photo of the business receipt.
+                var file = await CrossMedia.Current.TakePhotoAsync(mediaOptions);
+            }
+        }
+
+    }
+
+
+
+
+}
