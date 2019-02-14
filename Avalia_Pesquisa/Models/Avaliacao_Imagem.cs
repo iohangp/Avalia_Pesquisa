14f76c852bdf0c787e9a8b29a993dbc80c928565
Michael <m1xaeu@hotmail.com>
2019-02-13 22:34:58 -0200

    ajuste banco imagem

--- Avalia_Pesquisa/Models/Avaliacao_Imagem.cs
+++ Avalia_Pesquisa/Models/Avaliacao_Imagem.cs
@@ -7,6 +7,8 @@ namespace Avalia_Pesquisa
         public int IdAvaliacao_Imagem { get; set; }
         public string Imagem { get; set; }
         public int idAvaliacao { get; set; }
+        public int tratamento { get; set; }
+        public int repeticao { get; set; }
         public int idUsuario { get; set; }
         public DateTime Data { get; set; }
     }
